d8d987f0379681c613f22f170c142dfb77c96da4
ilsun choi <ilsun@ilsunui-MacBookPro.local>
2024-02-03 21:46:04 +0900

    search bar

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 
 browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-browser.get('https://google.com')
+SEARCH_ENGINE = 'https://google.com'
+KEYWORD = 'fmdskal'
+
+browser.get(SEARCH_ENGINE)
+
+
+search_bar = browser.find_element(By.CLASS_NAME,'gLFyf')
+search_bar.send_keys(KEYWORD)
+search_bar.send_keys(Keys.ENTER)
+
+print(search_bar)
+browser.quit()
